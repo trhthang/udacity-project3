378d726bef17e69b8873073fcdc0e176e2a85fad
Tran Huy Thang <tranhuythang@Trans-MacBook-Pro.local>
2022-11-04 00:38:04 +0700

    thang hihi

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=udagram-prj3-instance.ck3igafc7oym.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb
+export AWS_BUCKET=udagram-prj3-insta
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
